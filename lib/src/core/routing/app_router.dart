6c8dbd4fcfaf6c6247cfea31387b78361227f93d
NoHeroe <eco.noheroes@gmail.com>
2025-10-05 08:32:01 -0400

    Create app_router.dart

---
+++ lib/src/core/routing/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../presentation/splash/splash_page.dart';
+import '../../presentation/auth/login_page.dart';
+import '../../presentation/home/home_page.dart';
+
+final appRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const SplashPage(),
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => const HomePage(),
+      ),
+    ],
+  );
+});
